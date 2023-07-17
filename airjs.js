91886cbc732c4f59eb4a4f45decfc872af8a14dc
RealRiga <116930426+RealRiga@users.noreply.github.com>
2023-07-17 16:59:46 -0500

    did an adjustment on the css

---
+++ airjs.js
@@ -0,0 +1,13 @@
+function switchButton(){
+            
+            // console.log( document.getElementById('imgName'))
+                if(document.getElementById('imgName').src.includes('Captureoff')){
+                    document.getElementById('imgName').src = 'file:///C:/Users/ADEOLA%20YISA/Desktop/APTEC/Airtel%20Router/CaptureonR.png'
+                }else{
+                    document.getElementById('imgName').src ='C:/Users/ADEOLA%20YISA/Desktop/APTEC/Airtel%20Router/Captureoff.PNG'
+                }
+
+                
+        }
+
+        
